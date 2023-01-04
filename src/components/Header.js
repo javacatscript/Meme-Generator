fc3b6ceb976c914a4e31892f20dd7f18d47413ae
rupank <ru96nk@gmail.com>
2023-01-04 12:53:14 +0530

    adding meme component and styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,17 @@
 import React from "react";
-import '../style.css'
+import '../style.css';
 
 export default function Header(){
 
     return (
         <header className="header">
             <img 
-                src="../images/troll-face.png"
+                src=""
                 className="header--image"
-                alt="troll-face-img"
+                
             />
             <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
+            <h3 className="header--project">catman</h3>
         </header>
     )
 }
